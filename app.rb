3507c495e040eb286193791864e0099830c7e47c
herokuconnectsample <nisitabira@gmail.com>
2017-01-19 10:18:24 +0900

    #
    #

--- app.rb
+++ app.rb
@@ -18,15 +18,12 @@ get "/contacts" do
   @contacts = Contact.all
   
 #  @contact_21 = Contact.find_by(id: 21)
-  
-  logger.info('修正前')
-  logger.info(@contact_21.lastname)
-  
+#  logger.info('修正前')
+#  logger.info(@contact_21.lastname)
 #  @contact_21.lastname="99999999"
 #  @contact_21.save!
-
-  logger.info('修正後')
-  logger.info(@contact_21.lastname)
+#  logger.info('修正後')
+#  logger.info(@contact_21.lastname)
   
   erb :index
 end
